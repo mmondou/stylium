772a5e2c33995257610c1f46552f7f52631b598e
Michaël Mondou <michael.mondou@littleworker.fr>
2023-12-17 22:46:19 +0100

    ✨Simple footer

--- components/Footer.js
+++ components/Footer.js
@@ -2,6 +2,8 @@ import styles from './Footer.module.css'
 
 export default function Footer() {
   return (
-    <footer className={styles.footer}></footer>
+    <footer className={styles.footer}>
+      <p>Stylium</p>
+    </footer>
   )
 }
